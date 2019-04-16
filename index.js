d0c25c5f6251757ebc3a0afbc27a495c19daba25
itech2018 <37976121+itech2018@users.noreply.github.com>
2019-04-16 19:35:35 +0100

    Update and rename bot.js to index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+
+const { readFileSync } = require('fs')
+
+require('moment')
+require('moment-duration-format')
+
+// Initialize Canvas
+let canvasLoaded = false
+try {
+  require('canvas')
+  require('./src/utils/CanvasUtils.js').initializeHelpers()
+  canvasLoaded = true
+} catch (e) {}
+
+// Initialize client
+const CLIENT_OPTIONS = {
+  fetchAllMembers: true,
+  enableEveryone: false,
+  canvasLoaded
+}
+
+console.log(readFileSync('bigtitle.txt', 'utf8').toString())
+
+const Switchblade = require('./src/Switchblade.js')
+const client = new Switchblade(CLIENT_OPTIONS)
+client.login().then(() => client.log('[32mLogged in successfully!', 'Discord')).catch(e => client.logError(e))
